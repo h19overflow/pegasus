4e803b0f93867aca64efe65685062bc76b0aa372
jkishoret <jkishoret@gmail.com>
2026-03-05 22:48:55 -0700

    draft

--- montgomery-navigator/src/App.tsx
+++ montgomery-navigator/src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AppProvider } from "@/lib/appContext";
 import Splash from "./pages/Splash";
 import CommandCenter from "./pages/CommandCenter";
+import Notifications from "./pages/Notifications";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -19,6 +20,7 @@ const App = () => (
         <AppProvider>
           <Routes>
             <Route path="/" element={<Splash />} />
+            <Route path="/notifications" element={<Notifications />} />
             <Route path="/app" element={<Navigate to="/app/services" replace />} />
             <Route path="/app/:view" element={<CommandCenter />} />
             <Route path="/chat" element={<Navigate to="/app/services" replace />} />
